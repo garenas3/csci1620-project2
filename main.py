a2caaddc30213c35cbc29fd9eee0312477a4935a
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:15:07 -0600

    Set up main window widgets

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFormLayout, QHBoxLayout, QVBoxLayout, QPushButton
 
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.zip_code_edit = QLineEdit()
+        self.submit_button = QPushButton("Submit")
+        self.close_button = QPushButton("Close")
         self.setWindowTitle('Frost Dates')
+        self.setUpWidgets()
+
+    def setUpWidgets(self):
+        """Set up the widgets in the main window."""
+        form_layout = QFormLayout()
+        form_layout.addRow("Zip Code:", self.zip_code_edit)
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.submit_button)
+        buttons_layout.addWidget(self.close_button)
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(buttons_layout)
+        self.setLayout(main_layout)
 
 
 def main():
