fdb41c8400ea05667562202d290983a55c346d1d
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:51:00 -0600

    Add list widget

--- view.py
+++ view.py
@@ -1,11 +1,12 @@
 from PyQt5.QtWidgets import (QWidget, QLineEdit, QFormLayout, QHBoxLayout,
-                             QVBoxLayout, QPushButton)
+                             QVBoxLayout, QPushButton, QListWidget)
 
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.zip_code_edit = QLineEdit()
+        self.zip_code_list = QListWidget()
         self.submit_button = QPushButton("Submit")
         self.close_button = QPushButton("Close")
         self.setWindowTitle('Frost Dates')
@@ -20,5 +21,6 @@ class MainWindow(QWidget):
         buttons_layout.addWidget(self.close_button)
         main_layout = QVBoxLayout()
         main_layout.addLayout(form_layout)
+        main_layout.addWidget(self.zip_code_list)
         main_layout.addLayout(buttons_layout)
         self.setLayout(main_layout)
