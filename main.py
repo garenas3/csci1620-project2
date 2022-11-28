af9a4b44efc018034ee07b208b98d25536c86a4c
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:16:57 -0600

    Move MainWindow to view.py

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFormLayout, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication
 
-
-class MainWindow(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.zip_code_edit = QLineEdit()
-        self.submit_button = QPushButton("Submit")
-        self.close_button = QPushButton("Close")
-        self.setWindowTitle('Frost Dates')
-        self.setUpWidgets()
-
-    def setUpWidgets(self):
-        """Set up the widgets in the main window."""
-        form_layout = QFormLayout()
-        form_layout.addRow("Zip Code:", self.zip_code_edit)
-        buttons_layout = QHBoxLayout()
-        buttons_layout.addWidget(self.submit_button)
-        buttons_layout.addWidget(self.close_button)
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(form_layout)
-        main_layout.addLayout(buttons_layout)
-        self.setLayout(main_layout)
+from view import MainWindow
 
 
 def main():
