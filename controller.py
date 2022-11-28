8e69ebc59f9a6384b8e360a34cd268e73599253c
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:25:52 -0600

    Add submit_zip_code function

--- controller.py
+++ controller.py
@@ -13,3 +13,8 @@ class MainController:
     def set_up_signals_and_slots(self):
         """Set up the signals and slots for the program."""
         self.main_window.close_button.clicked.connect(self.main_window.close)
+
+
+def submit_zip_code(zip_code: str) -> None:
+    """Submit the zip code for processing."""
+    print(zip_code)
