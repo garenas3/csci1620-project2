f35be2bd9b79f06bf684e6297dbfd2e1dfb82a65
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:49:53 -0600

    Refactor

--- controller.py
+++ controller.py
@@ -13,12 +13,15 @@ class MainController:
     def set_up_signals_and_slots(self) -> None:
         """Set up the signals and slots for the program."""
         self.main_window.close_button.clicked.connect(self.main_window.close)
-        self.main_window.submit_button.clicked.connect(
-            lambda: submit_zip_code(self.main_window.zip_code_edit.text())
-        )
+        self.main_window.submit_button.clicked.connect(self.submit_zip_code)
         self.main_window.zip_code_edit.returnPressed.connect(
-            lambda: submit_zip_code(self.main_window.zip_code_edit.text())
+            self.submit_zip_code
         )
+
+    def submit_zip_code(self) -> None:
+        """Submit the zip code displayed in the zip code line edit."""
+        zip_code = self.main_window.zip_code_edit.text()
+        submit_zip_code(zip_code)
 
 
 def submit_zip_code(zip_code: str) -> None:
