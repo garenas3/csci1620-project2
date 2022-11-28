88a0df670fde9c4c36feebbeed9319e8d713179c
Gabe Arenas <garenas@unomaha.edu>
2022-11-28 13:26:15 -0600

    Add type hints to controller.py

--- controller.py
+++ controller.py
@@ -2,15 +2,15 @@ from view import MainWindow
 
 
 class MainController:
-    def __init__(self):
+    def __init__(self) -> None:
         self.main_window = MainWindow()
         self.set_up_signals_and_slots()
 
-    def show(self):
+    def show(self) -> None:
         """Show the main window to the user."""
         self.main_window.show()
 
-    def set_up_signals_and_slots(self):
+    def set_up_signals_and_slots(self) -> None:
         """Set up the signals and slots for the program."""
         self.main_window.close_button.clicked.connect(self.main_window.close)
 
