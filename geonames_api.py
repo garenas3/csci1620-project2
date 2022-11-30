76e9c317e7acfce9b038c3e97e846e5e153bcb36
Gabe Arenas <garenas@unomaha.edu>
2022-11-29 19:11:26 -0600

    Change singlequotes to double

--- geonames_api.py
+++ geonames_api.py
@@ -14,31 +14,31 @@ def get_zipcode_location(username: str, zipcode: str):
         The coordinates associated with the zip code.
     """
     payload = {                 # maxRows <= 500 uses 2 credits per request
-        'postalcode': zipcode,  # zip codes are exclusive to US
-        'country': 'US',        # restrict results to US
-        'radius': 30,           # 30 km is max radius for free accounts
-        'maxRows': 1,           # assume first row is correct latitude and longitude
-        'username': username    # username should be unique to application
+        "postalcode": zipcode,  # zip codes are exclusive to US
+        "country": "US",        # restrict results to US
+        "radius": 30,           # 30 km is max radius for free accounts
+        "maxRows": 1,           # assume first row is correct latitude and longitude
+        "username": username    # username should be unique to application
     }
-    r = requests.get('https://secure.geonames.org/findNearbyPostalCodesJSON', params=payload)
+    r = requests.get("https://secure.geonames.org/findNearbyPostalCodesJSON", params=payload)
     try:
         response = r.json()
         if not r.ok:
             if response:
-                error_message = response['status']['message']
-                error_code = response['status']['value']
-                raise Exception(f'GeoNames Webservice Exception ({error_code}): {error_message}')
+                error_message = response["status"]["message"]
+                error_code = response["status"]["value"]
+                raise Exception(f"GeoNames Webservice Exception ({error_code}): {error_message}")
             else:
                 r.raise_for_status()
-        result = response['postalCodes'][0]
-        return {'latitude': result['lat'], 'longitude': result['lng']}
+        result = response["postalCodes"][0]
+        return {"latitude": result["lat"], "longitude": result["lng"]}
     except requests.exceptions.JSONDecodeError:
-        raise Exception('Unable to parse JSON')
+        raise Exception("Unable to parse JSON")
 
 
 def load_username() -> str:
     """Load the GeoNames username from the file geonames.txt."""
-    with open('geonames.txt', 'r') as fh:
+    with open("geonames.txt", "r") as fh:
         return fh.read().strip()
 
 
