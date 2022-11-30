8dbd3d31b142657e50bbdbf5c984638e0af768fe
Gabe Arenas <garenas@unomaha.edu>
2022-11-30 11:52:00 -0600

    Convert to QMainWindow and add status bar

--- view.py
+++ view.py
@@ -1,20 +1,22 @@
 from PyQt5.QtWidgets import (QWidget, QLineEdit, QFormLayout, QHBoxLayout,
                              QVBoxLayout, QPushButton, QTreeWidget,
-                             QHeaderView)
+                             QHeaderView, QMainWindow)
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.zip_code_edit = QLineEdit()
         self.zip_code_list = QTreeWidget()
         self.submit_button = QPushButton("Submit")
         self.close_button = QPushButton("Close")
+        self.status_bar = self.statusBar()
         self.setWindowTitle('Frost Dates')
         self.setUpWidgets()
 
     def setUpWidgets(self):
         """Set up the widgets in the main window."""
+        central_widget = QWidget()
         form_layout = QFormLayout()
         form_layout.addRow("ZIP Code:", self.zip_code_edit)
         self.zip_code_list.setColumnCount(2)
@@ -28,4 +30,5 @@ class MainWindow(QWidget):
         main_layout.addLayout(form_layout)
         main_layout.addWidget(self.zip_code_list)
         main_layout.addLayout(buttons_layout)
-        self.setLayout(main_layout)
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
